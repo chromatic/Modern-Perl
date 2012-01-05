efb248eecdbd0fe6465114d703bfbb77af9fb6ae
chromatic <chromatic@wgz.org>
2012-01-04 21:00:58 -0800

    Updated documentation for new release.

--- lib/Modern/Perl.pm
+++ lib/Modern/Perl.pm
@@ -54,8 +54,33 @@ resolution order; see C<perldoc mro> for an explanation.  In the future, it
 will include additional CPAN modules which have proven useful and stable.
 
 See L<http://www.modernperlbooks.com/mt/2009/01/toward-a-modernperl.html> for
-more information, and L<http://www.modernperlbooks.com/> for further discussion
-of Modern Perl and its implications.
+more information, L<http://www.modernperlbooks.com/> for further discussion of
+Modern Perl and its implications, and
+L<http://onyxneon.com/books/modern_perl/index.html> for a freely-downloadable
+Modern Perl tutorial.
+
+=head2 Forward Compatibility
+
+For forward compatibility, I recommend you specify a I<year> as the single
+optional import tag. For example:
+
+    use Modern::Perl 2009;
+    use Modern::Perl 2010;
+
+... both enable 5.10 features, while:
+
+    use Modern::Perl 2011;
+
+... enables 5.12 features and:
+
+    use Modern::Perl 2012;
+
+... enables 5.14 features. Obviously you cannot use 5.14 features on earlier
+versions, and Perl will throw the appropriate exception if you try.
+
+In the future--probably the C<Modern::Perl> 2013 timeframe--this module may
+drop support for 5.10 and will complain (once per process) if you use a year
+too old.
 
 =head1 AUTHOR
 
@@ -103,16 +128,16 @@ L<http://search.cpan.org/dist/Modern-Perl/>
 
 Damian Conway (inspiration from L<Toolkit>), Florian Ragwitz
 (L<B::Hooks::Parser>, so I didn't have to write it myself), chocolateboy (for
-suggesting that I don't even need L<B::Hooks::Parser>, at least for now),
-Damien Learns Perl, David Moreno, and Evan Carroll for reporting bugs and
-requesting features.
+suggesting that I don't even need L<B::Hooks::Parser>), Damien Learns Perl,
+David Moreno, Evan Carroll, and Elliot Shank for reporting bugs and requesting
+features.
 
 =head1 COPYRIGHT & LICENSE
 
 Copyright 2009-2012 chromatic, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl 5.10 itself.
+under the same terms as Perl 5.14 itself.
 
 =cut
 
